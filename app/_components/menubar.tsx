5d636d6e776a3aa6a7cf027a52aab681d15a7725
NOOBCODER <erkhessw@gmail.com>
2026-04-01 21:08:55 +0800

    P

--- app/_components/menubar.tsx
+++ app/_components/menubar.tsx
@@ -1,7 +1,26 @@
 export default function MenuBar() {
   return (
-    <div className="w-[100vw] h-[10vh] text-[#008000]  font-bold">
-      <div className="text-[40px]">Erkhes</div>
+    <div className="w-[100vw] h-[10vh] text-[#008000] p-[20px] pl-[90px] justify-around flex font-bold">
+      <div className="flex items-center">
+        <div className="text-[40px] cursor-pointer">Erkhes</div>
+      </div>
+      <div className="flex justify-around items-center w-[70%]">
+        <div className="text-[15px] hover:underline cursor-pointer decoration-2 underline-offset-4">
+          About
+        </div>
+        <div className="text-[15px]  hover:underline cursor-pointer decoration-2 underline-offset-4 ">
+          Skills
+        </div>
+        <div className="text-[15px]  hover:underline cursor-pointer decoration-2 underline-offset-4 ">
+          Projects
+        </div>
+        <div className="text-[15px]  hover:underline cursor-pointer decoration-2 underline-offset-4">
+          Achievements
+        </div>
+        <div className="text-[15px] hover:underline cursor-pointer decoration-2 underline-offset-4">
+          Contact
+        </div>
+      </div>
     </div>
   );
 }
