5e35e2de20502b2d08126f63ca01552f083ad76d
NOOBCODER <erkhessw@gmail.com>
2026-04-06 14:36:58 +0800

    P

--- app/_components/menubar.tsx
+++ app/_components/menubar.tsx
@@ -1,8 +1,8 @@
 export default function MenuBar() {
   return (
-    <div className="w-[100vw] h-[10vh] text-[#008000] p-[20px] pl-[90px] justify-around flex font-bold">
+    <div className="w-full h-[10vh] text-[#008000] p-[20px] pl-[90px] justify-around flex font-bold">
       <div className="flex items-center">
-        <div className="text-[40px] cursor-pointer">Erkhes</div>
+        <div className="text-[40px] font-extrabold cursor-pointer">Erkhes</div>
       </div>
       <div className="flex justify-around items-center w-[70%]">
         <div className="text-[15px] hover:underline cursor-pointer decoration-2 underline-offset-4">
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,49 @@
+"use client";
+import { useEffect, useState } from "react";
 import MenuBar from "./_components/menubar";
 
 export default function Page() {
+  const [text, setText] = useState({
+    name: "Software Engineering",
+    color: "blue",
+  });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText((prev) =>
+        prev.color === "blue"
+          ? { name: "UI/UX design", color: "purple" }
+          : { name: "Software Engineering", color: "blue" },
+      );
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <div className="text-white">
+    <div className="">
       <div>
         <MenuBar />
+      </div>
+      <div className="w-[100vw] h-[90vh] flex justify-center items-center">
+        <div className="w-[80%] h-[80%] p-[10px] ">
+          <div className="w-[50%] h-[100%]">
+            <div className="text-[#008000] font-extrabold text-7xl flex flex-col gap-[15px]">
+              Hello, I am
+              <div>Erkhes B.</div>
+            </div>
+            <div className="text-white text-4xl font-extrabold">
+              {text.color === "blue" ? (
+                <div className="text-[#00b9ec]">
+                  {text.name}
+                  <span className="text-white">Student</span>
+                </div>
+              ) : (
+                <div className="te">{text.name} Student</div>
+              )}
+            </div>
+          </div>
+
+          <div></div>
+        </div>
       </div>
     </div>
   );
